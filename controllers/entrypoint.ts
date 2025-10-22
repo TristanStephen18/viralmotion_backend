204c069e743eac8a63fdfbe2e5a10310f52ac322
TristanStephen18 <trasco_21ur1406@psu.edu.ph>
2025-10-22 22:18:14 +0800

    entry point finding

--- controllers/entrypoint.ts
+++ controllers/entrypoint.ts
@@ -2,5 +2,14 @@ import path from "path";
 
 export const entry = path.join(
   process.cwd(),
-  "./server/remotion_templates/TemplateHolder/src/index.ts"
+  "./remotion_templates/TemplateHolder/src/index.ts"
 );
+
+
+export const entry2 = path.join(
+  process.cwd(), "remotion_templates/TemplateHolder/src/index.ts"
+)
+
+export const distentry = path.join(
+  process.cwd(), "dist/remotion_templates/TemplateHolder/src/index.ts"
+)
--- index.ts
+++ index.ts
@@ -5,16 +5,18 @@ import uploadroutes from "./routes/uploads.ts";
 import elevenlabsroutes from "./routes/apis/elevenlabs.ts";
 import redditroute from "./routes/apis/reddit.ts";
 import authroutes from "./routes/database/auth.ts";
-import projectsroutes from  "./routes/database/projects.ts";
+import projectsroutes from "./routes/database/projects.ts";
 import uploadindbroutes from "./routes/database/useruploads.ts";
 import pixabayroutes from "./routes/apis/pixabay.ts";
 import rendersroutes from "./routes/database/renders.ts";
 import datasetsdbupload from "./routes/database/datasetsupload.ts";
 import getDatasetFronUploadsroute from "./routes/apis/fromuploadsextraction.ts";
 import cors from "cors";
+import fs from "fs";
+import { distentry, entry, entry2 } from "./controllers/entrypoint.ts";
 
 const app = express();
-app.use(cors({origin: "*"}));
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,7 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // ViteExpress.config({ viteConfigFile: "./frontend/vite.config.ts" });
 app.set("trust proxy", true);
-
 
 // app.use('/data', dataroutes);
 app.use("/api", airoutes);
@@ -39,10 +40,13 @@ app.use("/renders", rendersroutes);
 app.use("/datasets", datasetsdbupload);
 app.use("/fromuploadsdataset", getDatasetFronUploadsroute);
 
-
 app.listen(3000, "0.0.0.0", () => {
   // console.log(__dirname);
   // console.log(geminiapi);
   // console.log(path.join(process.cwd(),"./server/public/datasets"));
+  fs.existsSync(entry);
+  fs.existsSync(entry2);
+  fs.existsSync(distentry);
+
   console.log("Server is running on http://0.0.0.0:3000");
 });
