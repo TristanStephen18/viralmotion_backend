ebce72031f4c4dbd871a0ed4306c5c7cb6aaf584
TristanStephen18 <trasco_21ur1406@psu.edu.ph>
2025-11-21 06:48:34 +0800

    changed entry point for render

--- controllers/entrypoint.ts
+++ controllers/entrypoint.ts
@@ -2,7 +2,7 @@ import path from "path";
 
 export const entry = path.join(
   process.cwd(),
-  "./server/remotion_templates/TemplateHolder/src/index.ts"
+  "./remotion_templates/TemplateHolder/src/index.ts"
 );
 
 
